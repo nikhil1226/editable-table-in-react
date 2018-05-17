1a70371e9204ebfc7206b1dbcd6957b21032d94e
K <578135@cognizant.com>
2018-05-17 19:16:47 +0530

    sf

--- src/App.js
+++ src/App.js
@@ -23,12 +23,19 @@ class App extends Component {
       ],
       name: '',
       number: '',
+      tname: '',
+      tnumber: '',
       hideAddBtn: false,
       index: 0,
       errorsArr: [],
       reqNameErr: false,
-      reqNumberErr: false
+      reqNumberErr: false,
+      flag: []
     };
+  }
+
+  componentWillMount() {
+
   }
 
   delete = (index) => {
@@ -42,15 +49,39 @@ class App extends Component {
   add = (event) => {
     event.preventDefault();
     const users1 = Object.assign([], this.state.dataArr);
-    if (!this.state.reqNameErr && !this.state.reqNumberErr) {
-      users1.push({
-        name: this.state.name,
-        number: this.state.number,
+    const flag1 = Object.assign([], this.state.flag);
+    if (!this.state.tname || this.state.tname === "") {
+      this.setState({
+        reqNameErr: true
       });
+    } else {
+      this.setState({
+        reqNameErr: false
+      });
+    }
+
+    if (!this.state.tnumber || this.state.tnumber === "") {
+      this.setState({
+        reqNumberErr: true
+      });
+    } else {
+      this.setState({
+        reqNumberErr: false
+      });
+    }
+
+    // !this.state.reqNameErr && !this.state.reqNumberErr
+    if (this.state.tname && this.state.tnumber) {
+      users1.push({
+        name: this.state.tname,
+        number: this.state.tnumber,
+      });
+      flag1.push(false);
       this.setState({
         dataArr: users1,
-        name: '',
-        number: ''
+        tname: '',
+        tnumber: '',
+        flag: flag1
       });
     } else {
       //
@@ -58,23 +89,25 @@ class App extends Component {
   }
 
   editBtn = (obj, i) => {
+    const flag2 = Object.assign([], this.state.flag);
+    for (let k = 0; k < this.state.dataArr.length; k++) {
+      flag2[k] = false;
+    }
+    flag2[i] = true;
     this.setState({
       name: obj.name,
       number: obj.number,
-      hideAddBtn: true,
-      index: i
-    });
-  }
-
-  editName = (event, index) => {
-    const users1 = Object.assign([], this.state.dataArr);
-    users1[index].name = event.target.value;
-    this.setState({
-      dataArr: users1
+      hideAddBtn: !this.state.hideAddBtn,
+      index: i,
+      flag: flag2
     });
   }
 
   save = () => {
+    const flag3 = Object.assign([], this.state.flag);
+    for (let k = 0; k < this.state.dataArr.length; k++) {
+      flag3[k] = false;
+    }
     const users2 = Object.assign([], this.state.dataArr);
     users2[this.state.index].name = this.state.name;
     users2[this.state.index].number = this.state.number;
@@ -82,35 +115,36 @@ class App extends Component {
       dataArr: users2,
       name: '',
       number: '',
-      hideAddBtn: false
+      hideAddBtn: false,
+      flag: flag3
     });
   }
 
   nameOnChange = (event) => {
     event.preventDefault();
-    this.setState({ name: event.target.value });
-    this.setState((prevState) => {
-      if (!prevState.name || prevState.name === "") {
-        return { reqNameErr: true }
-      } else {
-        return { reqNameErr: false }
-      }
-    });
+    this.setState({ tname: event.target.value });
+    // this.setState((prevState) => {
+    //   if (!prevState.name || prevState.name === "") {
+    //     return { reqNameErr: true }
+    //   } else {
+    //     return { reqNameErr: false }
+    //   }
+    // });    
   }
 
   numberOnChange = (event) => {
     if ((event.target.value).match(/^[0-9_ ]*$/)) {
       this.setState({
-        number: event.target.value
+        tnumber: event.target.value
       });
     }
-    this.setState(prevState => {
-      if (!prevState.number) {
-        return { reqNumberErr: true }
-      } else {
-        return { reqNumberErr: false }
-      }
-    });
+    // this.setState(prevState => {
+    //   if (!prevState.number) {
+    //     return { reqNumberErr: true }
+    //   } else {
+    //     return { reqNumberErr: false }
+    //   }
+    // });
   }
 
   render() {
@@ -132,42 +166,46 @@ class App extends Component {
               <tbody>
                 {this.state.dataArr.map((contact, index) => {
                   return (
-                    <tr key={contact.id}>
+                    <tr key={contact.name}>
                       <td>
-                        {contact.name}
+                        {!this.state.flag[index] && contact.name}
+                        {this.state.flag[index] && <input type="text" className="form-control"
+                          value={this.state.name} onChange={e => this.setState({name: e.target.value})} maxLength="15" required />}
                       </td>
                       <td>
-                        {contact.number}
+                        {!this.state.flag[index] && contact.number}
+                        {this.state.flag[index] && <input type="text" className="form-control"
+                          value={this.state.number} onChange={e => this.setState({number: e.target.value})} maxLength="15" required />}
                       </td>
                       <td>
-                        <button type="button" className="btn btn-info" onClick={this.editBtn.bind(this, contact, index)} >
+                        {!this.state.flag[index] && <button type="button" className="btn btn-info" onClick={this.editBtn.bind(this, contact, index)} >
                           <span className="glyphicon glyphicon-edit"></span>&nbsp;Edit
-						          	</button>
+						          	</button>}
                         &nbsp;&nbsp;
-                        <button type="button" className="btn btn-danger" onClick={this.delete.bind(this, index)}>
+                        {!this.state.flag[index] && <button type="button" className="btn btn-danger" onClick={this.delete.bind(this, index)}>
                           <span className="glyphicon glyphicon-trash"></span>&nbsp;Delete
-                        </button>
+                        </button>}
+                        {this.state.flag[index] && <button type="button" className="btn btn-primary" onClick={this.save}>
+                          <span className="glyphicon glyphicon-floppy-disk"></span>&nbsp;Save
+							          </button>}
                       </td>
                     </tr>)
                 })}
                 <tr style={{ height: '70px' }}>
                   <td>
-                    <input type="text" className="form-control" value={this.state.name} placeholder="Name"
+                    <input type="text" className="form-control" value={this.state.tname} placeholder="Name"
                       onChange={this.nameOnChange} maxLength="15" required />
                     {this.state.reqNameErr && <span style={{ color: "red" }}>This field can't be empty</span>}
                   </td>
                   <td>
-                    <input type="text" className="form-control" value={this.state.number} placeholder="Contact Number"
+                    <input type="text" className="form-control" value={this.state.tnumber} placeholder="Contact Number"
                       onChange={this.numberOnChange} maxLength="10" required />
                     {this.state.reqNumberErr && <span style={{ color: "red" }}>This field can't be empty</span>}
                   </td>
                   <td>
-                    {!this.state.hideAddBtn && <button type="submit" className="btn btn-primary" onClick={this.add}>
+                    <button type="submit" className="btn btn-primary" onClick={this.add}>
                       <span className="glyphicon glyphicon-plus"></span>&nbsp;Add
-								        </button>}
-                    {this.state.hideAddBtn && <button type="button" className="btn btn-primary" onClick={this.save}>
-                      <span className="glyphicon glyphicon-floppy-disk"></span>&nbsp;Save
-							          </button>}
+								        </button>
                   </td>
                 </tr>
               </tbody>
